feb69cf09f05859e43f5045825931af0c4aaa002
nick <nickswanenberg@gmail.com>
2025-09-26 12:04:32 +0200

    basic pages setup

--- App.js
+++ App.js
@@ -1,20 +1,56 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from "react";
+import { Button, View, Text, StyleSheet } from "react-native";
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+
+const Stack = createNativeStackNavigator();
+
+// Home Screen
+function HomeScreen({ navigation }) {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.title}>Home Screen</Text>
+      <Button
+        title="Go to Ride Page"
+        onPress={() => navigation.navigate("Ride")}
+      />
+    </View>
+  );
+}
+
+// Ride Screen
+function RideScreen({ navigation }) {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.title}>Ride Screen</Text>
+      <Button
+        title="Go back Home"
+        onPress={() => navigation.navigate("Home")}
+      />
+    </View>
+  );
+}
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName="Home">
+        <Stack.Screen name="Home" component={HomeScreen} />
+        <Stack.Screen name="Ride" component={RideScreen} />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  title: {
+    fontSize: 24,
+    marginBottom: 20,
   },
 });
+
